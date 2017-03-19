f0146ec26c5ae45113809d4decf495f11e7555c7
Mayank Kumar Jha <mk9440@users.noreply.github.com>
2017-03-19 15:42:47 +0530

    Add files via upload
    Video recorder + video player

---
+++ VideoPlayer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 03:55:06 2017
+
+@author: mayank
+"""
+
+import numpy
+import cv2
+
+cap=cv2.VideoCapture('E:/Anime/Gantz-Season-1/[LibreTanan.com] Gantz Season 1 Episode 01.avi')
+print("VideoPlayer")
+while(cap.isOpened()):
+    ret,frame=cap.read()
+    cv2.resizeWindow('Videoplayer',640,340)
+    cv2.imshow('VideoPlayer',frame)
+    if cv2.waitKey(25)==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()   
